55e8d2d62a45935294bc877266500d092a051eb6
Joseph Edusei <jedusei99@gmail.com>
2020-02-10 10:27:14 +0000

    Designed part of iOS settings screen.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,32 +2,44 @@ import React from 'react';
 import {
   StyleSheet,
   Text,
-  View,
+  View, Image
 } from 'react-native';
-
+import { Ionicons } from '@expo/vector-icons';
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.h1}>
-        Home
-     </Text>
+    <View style={{ backgroundColor: "#f3f3f3", flexDirection: 'column', flex: 1 }}>
+      <View>
+        <Text style={{ fontSize: 25, paddingHorizontal: 15, paddingVertical: 5 }}>Settings</Text>
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: 'center', backgroundColor: "white", elevation: 1 }}>
+        <View style={{ height: 100, flex: 4, justifyContent: "center", alignItems: "center" }}>
+          <Image source={require('../assets/images/simona-profile.jpg')} style={{ width: 80, height: 80, borderRadius: 100 }} />
+        </View>
+        <View style={{ height: 100, flex: 8, justifyContent: 'center', paddingLeft: 5 }}>
+          <Text style={{ fontSize: 20 }}>Joseph Edusei</Text>
+          <Text style={{ fontSize: 12, opacity: 0.5 }}>Apple ID, iCloud & App Store</Text>
+        </View>
+        <View style={{ height: 100, alignSelf: "flex-end", justifyContent: "center", marginRight: 10 }}>
+          <Ionicons name='ios-arrow-forward' size={24} />
+        </View>
+      </View>
+      <View style={{ backgroundColor: "white", marginTop: 15, paddingVertical: 10, flexDirection: "row", alignItems: 'center', elevation: 1 }}>
+        <Text style={{ marginLeft: 15, flex: 1 }}>Finish setting up your iPhone</Text>
+        <View style={{ backgroundColor: "red", marginHorizontal: 5, borderRadius: 10, width: 20, height: 20, alignItems: "center", justifyContent: "center" }}>
+          <Text style={{ color: "white" }}>1</Text>
+        </View>
+        <View style={{ alignSelf: "flex-end", marginRight: 10 }}>
+          <Ionicons name='ios-arrow-forward' size={24} />
+        </View>
+      </View>
     </View>
   );
 }
 
-HomeScreen.navigationOptions = {
-  header: null,
-};
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  h1: {
-    fontSize: 28
+  pic: {
+    borderRadius: 20,
+    borderColor: '#000'
   }
 });
